2fbc0c8daed95e91b5ad8ab9f556964614b125bc
victoria vilariño <victoria-vilarino@hotmail.com>
2017-10-10 17:54:14 -0300

    actividades del dia 10-10

---
+++ 10-10/array.js
@@ -0,0 +1,34 @@
+var notasMusicales= [];
+var nombre;
+nombre="Gia"
+
+var cantNombre=nombre.length;
+console.log("gia tiene letras cant:"+ cantNombre);
+
+notasMusicales[0]= "Do";
+notasMusicales[1]= "Re";
+notasMusicales[2]="Mi";
+notasMusicales[3]="Fa";
+notasMusicales[4]="Celes";
+
+var cant= notasMusicales.length;
+var ultimoElemento = notasMusicales.length-1;
+console.log("cant. notas:" + ultimoElemento);		
+
+console.log("el ultimo elemento es:" + notasMusicales[notasMusicales.length-1] );
+
+var colores= [];
+
+colores[0]= "rojo";
+colores[1]= "Azul";
+colores[2]= "Violeta";
+colores[3]= "Azul";
+colores[4]= "Amarillo";
+
+var cant=colores.length;
+
+var ultimoelemento= colores.length-1;
+console.log ("el ultimo elemento es:" + colores[colores.length-1])
+
+var primerElemento= colores.length-5;
+console.log ("el primer elemento es:" + colores[colores.length-5])
---
+++ Nueva carpeta/array.js
@@ -0,0 +1,14 @@
+/*var semana=   [50, 35, 105, 40,25]
+var suma=0
+
+for (var i =0; i>=(semana.length;) 1++)
+{console.log("posición"+1+"es"+semana[i])}
+console.log ("total de la semana"+semana) */
+
+var semana =  [50, 35, 105, 40, 25]
+var dias=   ["lunes","martes","miercoles","jueves","viernes"]
+for(i=0; i<=semana.length;i++)
+{if (semana  [i] >50 ) {
+	console.log (dias  [i]+":"+semana  [i])
+}
+}
